7fde43a118dcb4f160142c04f0a360107112d7d7
John Wood <john@signalhq.com>
2011-05-08 15:01:34 -0500

    Pull javascript into application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$("#say_form").bind("ajax:complete", function() {
+  $('#message').val('');
+});
+
+function playAudio(url) {
+  $("#audio_element").html("<embed src='" + url + "' hidden='true' autostart='true' loop='false' />");
+}
+
+function displayCaption(phrase) {
+  $("#phrase_text").text(phrase);
+  $("#phrase").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}).
+    delay(5000).
+    css({opacity: 1.0}).animate({opacity: 0.0});
+}
+
+function process(data) {
+  var dataObject = eval('(' + data + ')');
+  displayCaption(dataObject.phrase);
+  playAudio(dataObject.url);
+}
+
+var jug = new Juggernaut;
+jug.subscribe("speak_boc_speak", function(data) {
+  process(data);
+});
